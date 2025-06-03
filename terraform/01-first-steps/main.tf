a65008fba1e955fc50e290ad5dcd66287ae983ab
Sebastian Riede-Zwaetz <182764723+sebriede6@users.noreply.github.com>
2025-06-03 15:28:04 +0200

    Update main.tf

--- terraform/01-first-steps/main.tf
+++ terraform/01-first-steps/main.tf
@@ -1,13 +1,13 @@
 resource "docker_image" "nginx_image" {
-  name         = "nginx:1.27-alpine" # Spezifischere Version als nur :latest
-  keep_locally = false # Optional: Entfernt das Image lokal, wenn die Ressource zerstört wird
+  name         = "nginx:1.27-alpine" 
+  keep_locally = false 
 }
 
 resource "docker_container" "simple_nginx_container" {
   name  = "my-first-tf-nginx-container"
-  image = docker_image.nginx_image.image_id # Referenziert das Image über seine ID
+  image = docker_image.nginx_image.image_id 
   ports {
     internal = 80
-    # external = 8088 # Optional: Wenn du einen Port zum Host mappen willst
+    
   }
 }
